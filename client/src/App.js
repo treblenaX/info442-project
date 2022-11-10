76cd5871378d097f984777e3c59dbdc008075831
treblenaX <elbertkcheng@outlook.com>
2022-11-09 22:16:28 -0800

    Refactored the client organization

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,19 @@
-import React, { Component } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Section from "./components/Section";
-import dummyText from "./constants/DummyText";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <Section
-          title="Section 1"
-          subtitle={dummyText}
-          dark={true}
-          id="section1"
-        />
-        <Section
-          title="Section 2"
-          subtitle={dummyText}
-          dark={false}
-          id="section2"
-        />
-        <Section
-          title="Section 3"
-          subtitle={dummyText}
-          dark={true}
-          id="section3"
-        />
-        <Section
-          title="Section 4"
-          subtitle={dummyText}
-          dark={false}
-          id="section4"
-        />
-        <Section
-          title="Section 5"
-          subtitle={dummyText}
-          dark={true}
-          id="section5"
-        />
+import React, { Component, useEffect } from "react";
+import "./styles/App.css";
+
+
+function App() {
+  useEffect(() => {
+      // This is where you should load the data
+  }, []);
+
+  return (
+      <div>
+        <h1>
+          Hi!
+        </h1>
       </div>
-    );
-  }
+  )
 }
 
 export default App;
