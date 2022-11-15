f1140ba3e7d041db4b98abaf8c57fe2c6f77ba62
keith ellingwood <keith-3@hotmail.com>
2022-11-15 14:27:38 -0800

    added dummy point to map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,12 +4,27 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
 let dummy_data = {
+    location_type: 'BUILDING',
+    address: '4060 George Washington Lane Northeast, Seattle, WA 98195',
+    latitude: -122.31036845141246,
+    longitude: 47.656471273388775,
+    name: 'Odegaard Undergraduate Library',
+    average_rating: 4.5,
+    picture_urls: []
+}
 
+let placeholder = {
+    geo: {
+        type: "Feature",
+        geometry: {
+            type: "Point",
+            coordinates: [-122.31036845141246, 47.656471273388775]
+        }
+    }
 }
 
 function DisplayMap() 
 {
-    console.log(process.env)
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [lng, setLng] = useState(-122.30808827297321);
@@ -27,15 +42,14 @@ function DisplayMap()
         });
     });
 
-    // reset map values when changed by user
+    // populate building points
     useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    });
+        let el = document.createElement('div');
+        el.className = 'marker';
+
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el).setLngLat([dummy_data.latitude, dummy_data.longitude]).addTo(map.current);
+    })
 
     return (
         <div>
