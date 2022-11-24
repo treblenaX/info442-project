08c5bad9d6532ced752f76781fe74d01ad889970
treblenaX <elbertkcheng@outlook.com>
2022-11-23 16:26:26 -0800

    Refactored login page to React bootstrap

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,41 +1,63 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
 
-function LoginForm({ Login, error }) {
-    const [details, setDetails] = useState({name: "", email: "", password: ""});
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-
-    const submitHandler = e => {
+    const handleSubmit = (e) => {
         e.preventDefault();
 
-        Login(details);
+        const form = {
+            username: username,
+            password: password
+        };
+
+        console.log(form);
     }
 
-
-
     return (
-        <form onSubmit={submitHandler}>
-            <div className='form-inner'>
-                <h2>Login</h2>
-                {(error !="") ? ( <div className="error">{error}</div>) : ""}
-                <div className="form-group">
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-                </div>
-                <div className='form-group'>
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-                </div>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group size="lg" className="mb-3">
+                <Form.Label>Username</Form.Label>
+                <Form.Control 
+                    type="username" 
+                    placeholder="ex: echeng23"
+                    onChange={(e) => setUsername(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+            <Form.Group size="lg" className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
+                    type="password"
+                    placeholder="Enter your password..."
+                    onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+            </Form.Group>
+            <Button type="submit" variant="primary">
+                Submit
+            </Button>
+        </Form>
+        // <form onSubmit={submitHandler}>
+        //     <div className='form-inner'>
+        //         <h2>Login</h2>
+        //         {(error !="") ? ( <div className="error">{error}</div>) : ""}
+        //         <div className="form-group">
+        //             <label htmlFor="name">Name: </label>
+        //             <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+        //         </div>
+        //         <div className='form-group'>
+        //             <label htmlFor="email">Email: </label>
+        //             <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+        //         </div>
 
-                <div className='form-group'>
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-                </div>
-                <input type="submit" value="Login" />
+        //         <div className='form-group'>
+        //             <label htmlFor="password">Password: </label>
+        //             <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+        //         </div>
+        //         <input type="submit" value="Login" />
 
-            </div>
-        </form>
+        //     </div>
+        // </form>
     )
 }
-
-export default LoginForm;
